9ec9bef2729e19c425187ed572657dbff294d699
nakoruru <nako_ruru@163.com>
2017-08-19 15:55:21 +0800

    reuse amqp

--- src/server/connector/internal/service/connectsvc/connectsvc.go
+++ src/server/connector/internal/service/connectsvc/connectsvc.go
@@ -36,6 +36,9 @@ func failOnError(err error, msg string) {
 	}
 }
 
+var q amqp.Queue
+var ch *amqp.Channel
+
 type service struct {
 	*bdmsg.Server
 	clientM *ClientManager
@@ -194,22 +197,24 @@ func (s *service) handleMsg(ctx context.Context, p *bdmsg.Pumper, t bdmsg.MsgTyp
 
 		client.RPush(roomId, bytes)
 	} else {
-		conn, err := amqp.Dial("amqp://live_stream:BrightHe0@47.92.98.23:5672/")
-		failOnError(err, "Failed to connect to RabbitMQ")
-		defer conn.Close()
-		ch, err := conn.Channel()
-		failOnError(err, "Failed to open a channel")
-		defer ch.Close()
+		var err error
+		if ch == nil {
+			var conn *amqp.Connection
+			conn, err = amqp.Dial("amqp://live_stream:BrightHe0@47.92.98.23:5672/")
+			failOnError(err, "Failed to connect to RabbitMQ")
+			ch, err = conn.Channel()
+			failOnError(err, "Failed to open a channel")
 
-		q, err := ch.QueueDeclare(
-			"connector", // name
-			false,   // durable
-			false,   // delete when unused
-			false,   // exclusive
-			false,   // no-wait
-			nil,     // arguments
-		)
-		failOnError(err, "Failed to declare a queue")
+			q, err = ch.QueueDeclare(
+				"connector_test", // name
+				false,   // durable
+				false,   // delete when unused
+				false,   // exclusive
+				false,   // no-wait
+				nil,     // arguments
+			)
+			failOnError(err, "Failed to declare a queue")
+		}
 
 		body := string(bytes)
 		err = ch.Publish(
